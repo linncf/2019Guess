acc0d7229fcbf2f611b9b5427d880f6cee807c1d
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-09-18 13:33:45 +0200

    first draft of functionnal game

--- game.js
+++ game.js
@@ -1,61 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const DEFAULT_PORT = 8080;
 
 const MAX = 100;
 const MIN = 1;
 
-let pickedNumber = null
+const OK = 200;
+const WIN = 200;
+const LOWER = 201;
+const BIGGER = 202;
+const OVER = 203;
+const OUT_OF_BOUNDS = 204;
+const ERROR = 404;
 
-// GET, POST, PUT, DELETE
+let pickedNumber = null;
+let winner = false;
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || DEFAULT_PORT));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-
-
-app.get("/number",function(req,respons,next){
-    pickANumber()
-    console.log("Number is " + pickedNumber);
-    respons.json({code:"ok", min:MIN, max:MAX});
-})
-
-// Anything that follows /guess/ is a variable
-app.post("/guess/:number", (request,res) =>{
-    if(pickedNumber){
-        let guess = request.params.number;
-        let responsObj = (guess === pickedNumber) ? {code:"winner"}:{code:"nope"}
-        //return
-        res.json(responsObj);
-    }else{
-        res.status(404).json({code:404, msg:"Stuff happend.."})
+app.get("/start", function (req, response) {
+    if (!pickedNumber) {
+        pickedNumber = Math.floor(Math.random() * (MAX - MIN)) + MIN;
     }
-    
-})
 
+    response.json({code: OK, min: MIN, max: MAX});
+});
 
-function pickANumber(){
-    if(!pickedNumber){
-        pickedNumber = Math.round(Math.random() * (MAX-MIN))+MIN;
+app.post("/guess/:number", (request, res) => {
+    let responseObj = {code: ERROR, msg: "Game not started. Go to /start"};
+    if (pickedNumber) {
+        if (!winner) {
+            let guess = parseInt(request.params.number);
+
+            if (guess < MIN || guess > MAX) {
+                responseObj = {code: OUT_OF_BOUNDS, msg: "You are out of bounds! Check on /start"};
+            } else if (guess === pickedNumber) {
+                winner = true;
+                responseObj = {code: WIN, msg: "You guessed correctly ! Game over."};
+            } else if (guess < pickedNumber) {
+                responseObj = {code: LOWER, msg: "The number is bigger, try again!"};
+            } else {
+                responseObj = {code: BIGGER, msg: "The number is lower, try again!"};
+            }
+        } else {
+            responseObj = {code: OVER, msg: "The game is already over, too bad."};
+        }
+        res.json(responseObj);
+    } else {
+        res.status(ERROR).json(responseObj);
     }
-}
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(app.get('port'), function () { console.log('server running', app.get('port'));});
+app.listen(app.get('port'), function () {
+    console.log('server running', app.get('port'));
+});
